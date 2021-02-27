f72de37ee954cef274615345fb7f4755e57ad94e
Naomy Ackerman <ackermannaomy@gmail.com>
2021-02-27 16:23:09 +0700

    config mongodb engine

--- analisis/settings.py
+++ analisis/settings.py
@@ -74,10 +74,16 @@ WSGI_APPLICATION = 'analisis.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'djongo',
+        'NAME': 'analisis_sentiment',
     }
 }
 
